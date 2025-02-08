6849fdf89a48008ecc167a9b4842da3394d89c7d
Chicky <84818342+ChickyBoyOne@users.noreply.github.com>
2025-02-08 15:54:58 -0600

    Add instructions for `telegram_secrets.py`

--- telegram_secrets.py
+++ telegram_secrets.py
@@ -1,2 +1,4 @@
+# Enter these values from: https://my.telegram.org/apps
+# Log in => API development tools
 API_ID = "REPLACE THESE WORDS WITH YOUR API_ID"
 API_HASH = "REPLACE THESE WORDS WITH YOUR API_HASH"
